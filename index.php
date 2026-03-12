336ffd02f9df43f3a290941c2b941618b63a0157
escall-dev <joerenzescallente027@gmail.com>
2026-03-12 21:57:31 +0800

    fix: update page title for clarity

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SDO San Pedro City DTR System</title>
+    <title>SDO San Pedro City Click Time</title>
     <style>
         * {
             margin: 0;
